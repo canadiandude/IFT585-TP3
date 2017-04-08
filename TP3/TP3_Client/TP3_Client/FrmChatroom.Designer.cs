716bbc4e9052e6a9ad80abd7cc42b6081d4b143f
corentinmoiny <corentin.moiny.14@gmail.com>
2017-04-08 13:30:13 -0400

    CHatroomUI

--- TP3/TP3_Client/TP3_Client/FrmChatroom.Designer.cs
+++ TP3/TP3_Client/TP3_Client/FrmChatroom.Designer.cs
@@ -29,10 +29,144 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.lbUsers = new System.Windows.Forms.ListBox();
+            this.lbRooms = new System.Windows.Forms.ListBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lblRoomTitle = new System.Windows.Forms.Label();
+            this.lblUsers = new System.Windows.Forms.Label();
+            this.lblRooms = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.btnSend = new System.Windows.Forms.Button();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtSend = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // lbUsers
+            // 
+            this.lbUsers.FormattingEnabled = true;
+            this.lbUsers.ItemHeight = 16;
+            this.lbUsers.Location = new System.Drawing.Point(12, 28);
+            this.lbUsers.Name = "lbUsers";
+            this.lbUsers.Size = new System.Drawing.Size(226, 244);
+            this.lbUsers.TabIndex = 0;
+            // 
+            // lbRooms
+            // 
+            this.lbRooms.FormattingEnabled = true;
+            this.lbRooms.ItemHeight = 16;
+            this.lbRooms.Location = new System.Drawing.Point(12, 294);
+            this.lbRooms.Name = "lbRooms";
+            this.lbRooms.Size = new System.Drawing.Size(226, 244);
+            this.lbRooms.TabIndex = 1;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(12, 544);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(131, 31);
+            this.btnCreate.TabIndex = 2;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(149, 544);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(131, 31);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // lblRoomTitle
+            // 
+            this.lblRoomTitle.AutoSize = true;
+            this.lblRoomTitle.Location = new System.Drawing.Point(245, 12);
+            this.lblRoomTitle.Name = "lblRoomTitle";
+            this.lblRoomTitle.Size = new System.Drawing.Size(35, 17);
+            this.lblRoomTitle.TabIndex = 4;
+            this.lblRoomTitle.Text = "Title";
+            // 
+            // lblUsers
+            // 
+            this.lblUsers.AutoSize = true;
+            this.lblUsers.Location = new System.Drawing.Point(13, 11);
+            this.lblUsers.Name = "lblUsers";
+            this.lblUsers.Size = new System.Drawing.Size(49, 17);
+            this.lblUsers.TabIndex = 5;
+            this.lblUsers.Text = "Users:";
+            // 
+            // lblRooms
+            // 
+            this.lblRooms.AutoSize = true;
+            this.lblRooms.Location = new System.Drawing.Point(13, 275);
+            this.lblRooms.Name = "lblRooms";
+            this.lblRooms.Size = new System.Drawing.Size(56, 17);
+            this.lblRooms.TabIndex = 6;
+            this.lblRooms.Text = "Rooms:";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(689, 544);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(111, 31);
+            this.btnSend.TabIndex = 9;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(286, 544);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(397, 31);
+            this.txtSend.TabIndex = 11;
+            this.txtSend.Text = "";
+            // 
+            // FrmChatroom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "FrmChatroom";
+            this.ClientSize = new System.Drawing.Size(812, 587);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.lblRooms);
+            this.Controls.Add(this.lblUsers);
+            this.Controls.Add(this.lblRoomTitle);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.lbRooms);
+            this.Controls.Add(this.lbUsers);
+            this.Name = "FrmChatroom";
+            this.Text = "ChatroomFever";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lbUsers;
+        private System.Windows.Forms.ListBox lbRooms;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label lblRoomTitle;
+        private System.Windows.Forms.Label lblUsers;
+        private System.Windows.Forms.Label lblRooms;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.RichTextBox txtSend;
     }
 }
