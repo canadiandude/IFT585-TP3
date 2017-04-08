a63f5e457bd7eab43a578412dbc860c91f09da5c
canadiandude <francoisrioux67@hotmail.com>
2017-04-08 14:25:28 -0400

    server startup from args

--- TP3/TP3_Serveur/TP3_Serveur/Program.cs
+++ TP3/TP3_Serveur/TP3_Serveur/Program.cs
@@ -11,7 +11,7 @@ namespace TP3_Serveur
         static void Main(string[] args)
         {
             Server server = new Server();
-            server.Start("127.0.0.1", 42);
+            server.Start(args[0], int.Parse(args[1]));
             while (true)
             {
                 server.ReceiveFromClients();
