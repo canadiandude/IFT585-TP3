1aa6cdf6c0d7d23a8f4313c0677645b956a52865
Alexandre Ricard Lalonde <alexandre.ricard.lalonde@gmail.com>
2017-04-08 15:07:33 -0400

    added listview chatbox

--- TP3/TP3_Client/TP3_Client/FrmChatroom.Designer.cs
+++ TP3/TP3_Client/TP3_Client/FrmChatroom.Designer.cs
@@ -40,40 +40,43 @@
             this.btnSend = new System.Windows.Forms.Button();
             this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.txtSend = new System.Windows.Forms.RichTextBox();
+            this.ChatBox = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
             // lbUsers
             // 
             this.lbUsers.FormattingEnabled = true;
-            this.lbUsers.ItemHeight = 16;
-            this.lbUsers.Location = new System.Drawing.Point(12, 28);
+            this.lbUsers.Location = new System.Drawing.Point(9, 23);
+            this.lbUsers.Margin = new System.Windows.Forms.Padding(2);
             this.lbUsers.Name = "lbUsers";
-            this.lbUsers.Size = new System.Drawing.Size(226, 244);
+            this.lbUsers.Size = new System.Drawing.Size(170, 199);
             this.lbUsers.TabIndex = 0;
             // 
             // lbRooms
             // 
             this.lbRooms.FormattingEnabled = true;
-            this.lbRooms.ItemHeight = 16;
-            this.lbRooms.Location = new System.Drawing.Point(12, 294);
+            this.lbRooms.Location = new System.Drawing.Point(9, 239);
+            this.lbRooms.Margin = new System.Windows.Forms.Padding(2);
             this.lbRooms.Name = "lbRooms";
-            this.lbRooms.Size = new System.Drawing.Size(226, 244);
+            this.lbRooms.Size = new System.Drawing.Size(170, 199);
             this.lbRooms.TabIndex = 1;
             // 
             // btnCreate
             // 
-            this.btnCreate.Location = new System.Drawing.Point(12, 544);
+            this.btnCreate.Location = new System.Drawing.Point(9, 442);
+            this.btnCreate.Margin = new System.Windows.Forms.Padding(2);
             this.btnCreate.Name = "btnCreate";
-            this.btnCreate.Size = new System.Drawing.Size(131, 31);
+            this.btnCreate.Size = new System.Drawing.Size(98, 25);
             this.btnCreate.TabIndex = 2;
             this.btnCreate.Text = "Create";
             this.btnCreate.UseVisualStyleBackColor = true;
             // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(149, 544);
+            this.btnSearch.Location = new System.Drawing.Point(112, 442);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(2);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(131, 31);
+            this.btnSearch.Size = new System.Drawing.Size(98, 25);
             this.btnSearch.TabIndex = 3;
             this.btnSearch.Text = "Search";
             this.btnSearch.UseVisualStyleBackColor = true;
@@ -81,27 +84,30 @@
             // lblRoomTitle
             // 
             this.lblRoomTitle.AutoSize = true;
-            this.lblRoomTitle.Location = new System.Drawing.Point(245, 12);
+            this.lblRoomTitle.Location = new System.Drawing.Point(184, 10);
+            this.lblRoomTitle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblRoomTitle.Name = "lblRoomTitle";
-            this.lblRoomTitle.Size = new System.Drawing.Size(35, 17);
+            this.lblRoomTitle.Size = new System.Drawing.Size(27, 13);
             this.lblRoomTitle.TabIndex = 4;
             this.lblRoomTitle.Text = "Title";
             // 
             // lblUsers
             // 
             this.lblUsers.AutoSize = true;
-            this.lblUsers.Location = new System.Drawing.Point(13, 11);
+            this.lblUsers.Location = new System.Drawing.Point(10, 9);
+            this.lblUsers.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblUsers.Name = "lblUsers";
-            this.lblUsers.Size = new System.Drawing.Size(49, 17);
+            this.lblUsers.Size = new System.Drawing.Size(37, 13);
             this.lblUsers.TabIndex = 5;
             this.lblUsers.Text = "Users:";
             // 
             // lblRooms
             // 
             this.lblRooms.AutoSize = true;
-            this.lblRooms.Location = new System.Drawing.Point(13, 275);
+            this.lblRooms.Location = new System.Drawing.Point(10, 223);
+            this.lblRooms.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblRooms.Name = "lblRooms";
-            this.lblRooms.Size = new System.Drawing.Size(56, 17);
+            this.lblRooms.Size = new System.Drawing.Size(43, 13);
             this.lblRooms.TabIndex = 6;
             this.lblRooms.Text = "Rooms:";
             // 
@@ -113,9 +119,10 @@
             // 
             // btnSend
             // 
-            this.btnSend.Location = new System.Drawing.Point(689, 544);
+            this.btnSend.Location = new System.Drawing.Point(517, 442);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(2);
             this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(111, 31);
+            this.btnSend.Size = new System.Drawing.Size(83, 25);
             this.btnSend.TabIndex = 9;
             this.btnSend.Text = "Send";
             this.btnSend.UseVisualStyleBackColor = true;
@@ -128,17 +135,27 @@
             // 
             // txtSend
             // 
-            this.txtSend.Location = new System.Drawing.Point(286, 544);
+            this.txtSend.Location = new System.Drawing.Point(214, 442);
+            this.txtSend.Margin = new System.Windows.Forms.Padding(2);
             this.txtSend.Name = "txtSend";
-            this.txtSend.Size = new System.Drawing.Size(397, 31);
+            this.txtSend.Size = new System.Drawing.Size(299, 26);
             this.txtSend.TabIndex = 11;
             this.txtSend.Text = "";
             // 
+            // ChatBox
+            // 
+            this.ChatBox.Location = new System.Drawing.Point(187, 26);
+            this.ChatBox.Name = "ChatBox";
+            this.ChatBox.Size = new System.Drawing.Size(410, 411);
+            this.ChatBox.TabIndex = 12;
+            this.ChatBox.UseCompatibleStateImageBehavior = false;
+            // 
             // FrmChatroom
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(812, 587);
+            this.ClientSize = new System.Drawing.Size(609, 477);
+            this.Controls.Add(this.ChatBox);
             this.Controls.Add(this.txtSend);
             this.Controls.Add(this.btnSend);
             this.Controls.Add(this.lblRooms);
@@ -148,6 +165,7 @@
             this.Controls.Add(this.btnCreate);
             this.Controls.Add(this.lbRooms);
             this.Controls.Add(this.lbUsers);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FrmChatroom";
             this.Text = "ChatroomFever";
             this.ResumeLayout(false);
@@ -168,5 +186,6 @@
         private System.Windows.Forms.Button btnSend;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
         private System.Windows.Forms.RichTextBox txtSend;
+        private System.Windows.Forms.ListView ChatBox;
     }
 }
--- TP3/TP3_Client/TP3_Client/FrmChatroom.cs
+++ TP3/TP3_Client/TP3_Client/FrmChatroom.cs
@@ -15,6 +15,25 @@ namespace TP3_Client
         public FrmChatroom()
         {
             InitializeComponent();
+            InitChatBox();
         }
+        private void InitChatBox()
+        {
+            ChatBox.View = View.Details;
+            // Add a column with width 20 and left alignment.
+            ChatBox.Columns.Add("Utilisateur", ChatBox.Size.Width*20/100, HorizontalAlignment.Left);
+            ChatBox.Columns.Add("Date", ChatBox.Size.Width*15/100, HorizontalAlignment.Left);
+            ChatBox.Columns.Add("Message", ChatBox.Size.Width * 55 / 100, HorizontalAlignment.Left);
+            ChatBox.Columns.Add("Like", ChatBox.Size.Width * 10 / 100, HorizontalAlignment.Left);
+            AddMessage("Alex",DateTime.Now,"test",3);
+        }
+        public void AddMessage(String User,DateTime time,String Message,int like)
+        {
+            string[] row = { User, time.ToString() ,Message,like.ToString()};
+            var listViewItem = new ListViewItem(row);
+            ChatBox.Items.Add(listViewItem);
+            //ChatBox.Items.Add("test");
+        }
+
     }
 }
